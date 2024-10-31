39b505652120415229818112c16d75b8015ca5ac
DarkCard1nal <34416583+DarkCard1nal@users.noreply.github.com>
2024-10-31 08:41:03 +0200

    Create repository 	new file:   .rubocop.yml 	new file:   .ruby-version 	new file:   Gemfile 	new file:   Gemfile.lock 	new file:   constants.rb 	new file:   program.rb

---
+++ constants.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module Constants
+	AUTHOR = '"Tasklist" by Shkilnyi V. CS31'
+	FILE_PATH = ''
+end
---
+++ program.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require_relative 'constants'
+
+file_path = Constants::FILE_PATH
+file_path = ARGV[0] unless ARGV.empty?
